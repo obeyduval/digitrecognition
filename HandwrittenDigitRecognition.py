a72407a45e9fc03810ed7157c3f068165977b0ad
obeyduval <C22Timothy.Jackson@usafa.edu>
2021-09-08 11:05:26 -0600

    1.2
    transforms, initializing data, images and labels

--- HandwrittenDigitRecognition.py
+++ HandwrittenDigitRecognition.py
@@ -13,6 +13,21 @@ from torch import nn, optim
 # toTensor - converts the image into numbers
 # normalize - normalize tensor with a mean and stdv
 
-transform = transforms.Compose([transforms.ToTensor(),
-                              transforms.Normalize((0.5,), (0.5,)),
-                              ])
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,)), ])
+
+# batch size is the number of images we read in on one go
+# train set is what the network is trained on
+# val set is what the network will be tested on
+
+trainset = datasets.MNIST('PATH_TO_STORE_TRAINSET', download=True, train=True, transform=transform)
+valset = datasets.MNIST('PATH_TO_STORE_TESTSET', download=True, train=False, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+valloader = torch.utils.data.DataLoader(valset, batch_size=64, shuffle=True)
+
+# images and labels
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
+print(images.shape)
+print(labels.shape)
+
+
