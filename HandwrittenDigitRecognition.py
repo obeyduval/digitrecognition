f6399f8c2e1a1c5ba2c8b7cfa4bb0d6cb3a2a282
obeyduval <C22Timothy.Jackson@usafa.edu>
2021-09-08 10:57:54 -0600

    1.1
    transform

--- HandwrittenDigitRecognition.py
+++ HandwrittenDigitRecognition.py
@@ -1,3 +1,18 @@
 # Handwritten Digit Recognition Using PyTorch
 
-print("helloworld")
+# Pytorch Libraries and MNIST Database
+
+import numpy as np
+import torch
+import torchvision
+import matplotlib.pyplot as plt
+from time import time
+from torchvision import datasets, transforms
+from torch import nn, optim
+
+# toTensor - converts the image into numbers
+# normalize - normalize tensor with a mean and stdv
+
+transform = transforms.Compose([transforms.ToTensor(),
+                              transforms.Normalize((0.5,), (0.5,)),
+                              ])
