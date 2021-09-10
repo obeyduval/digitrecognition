57228152ea57606ca3d094410f832366596e2f62
obeyduval <C22Timothy.Jackson@usafa.edu>
2021-09-10 11:06:58 -0600

    1.5
    1.5

---
+++ ConvolutionNetwork.py
@@ -0,0 +1,50 @@
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+batch_size = 4
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                          shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                         shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+
+
+# functions to show an image
+
+
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+if __name__ == '__main__':
+
+#     # get some random training images
+#     dataiter = iter(trainloader)
+#     images, labels = dataiter.next()
+#
+#     # show images
+#     imshow(torchvision.utils.make_grid(images))
+#     # print labels
+#     print(' '.join('%5s' % classes[labels[j]] for j in range(batch_size)))
+
