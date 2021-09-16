7144c43bdb4d7f53779ff6a357100f633a4e42ce
obeyduval <C22Timothy.Jackson@usafa.edu>
2021-09-16 10:11:50 -0600

    4.2

---
+++ HandwrittenConvolutional.py
@@ -0,0 +1,126 @@
+# Handwritten Digit Recognition Using PyTorch
+# ** need to save model
+
+# Pytorch Libraries and MNIST Database
+
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+import numpy as np
+import torch
+import torchvision
+import matplotlib.pyplot as plt
+from time import time
+from torchvision import datasets, transforms
+from torch import nn, optim
+
+# toTensor - converts the image into numbers
+# normalize - normalize tensor with a mean and stdv
+
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,)), ])
+
+# batch size is the number of images we read in on one go
+# train set is what the network is trained on
+# val set is what the network will be tested again on
+
+trainset = datasets.MNIST('PATH_TO_STORE_TRAINSET', download=True, train=True, transform=transform)
+valset = datasets.MNIST('PATH_TO_STORE_TESTSET', download=True, train=False, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+valloader = torch.utils.data.DataLoader(valset, batch_size=64, shuffle=True)
+
+# images and labels (pull in one batch at a time)
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
+print(images.shape)
+print(labels.shape)
+
+# display one image
+# plt.imshow(images[0].numpy().squeeze(), cmap='gray_r')
+# plt.show()
+
+# display multiple images
+figure = plt.figure()
+num_of_images = 60
+for index in range(1, num_of_images + 1):
+    plt.subplot(6, 10, index)
+    plt.axis('off')
+    plt.imshow(images[index].numpy().squeeze(), cmap='gray_r')
+plt.show()
+
+# NETWORK IS INPUT LAYER, 2 HIDDEN LAYERS, and OUTPUT LAYER
+
+input_size = 784  # input layer
+hidden_sizes = [128, 64]  # hidden layers
+output_size = 10  # output layer
+
+# set up model
+model = nn.Sequential(nn.Linear(input_size, hidden_sizes[0]),
+                      nn.ReLU(),
+                      nn.Linear(hidden_sizes[0], hidden_sizes[1]),
+                      nn.ReLU(),
+                      nn.Linear(hidden_sizes[1], output_size),
+                      nn.LogSoftmax(dim=1))
+print(model)
+
+# criterion is the negative log likelihood loss
+criterion = nn.NLLLoss()
+images, labels = next(iter(trainloader))
+images = images.view(images.shape[0], -1)
+
+logps = model(images)  # log probabilities
+loss = criterion(logps, labels)  # calculate the NLL loss
+
+# weights before backward prop
+print('Before backward pass: \n', model[0].weight.grad)
+loss.backward()
+
+# weights after back prop
+print('After backward pass: \n', model[0].weight.grad)
+
+# training and updating weights
+optimizer = optim.SGD(model.parameters(), lr=0.003, momentum=0.9)
+time0 = time()
+epochs = 15
+for e in range(epochs):
+    running_loss = 0
+    for images, labels in trainloader:
+        # Flatten MNIST images into a 784 long vector
+        images = images.view(images.shape[0], -1)
+
+        # Training pass
+        optimizer.zero_grad()
+
+        output = model(images)
+        loss = criterion(output, labels)
+
+        # This is where the model learns by backpropagating
+        loss.backward()
+
+        # And optimizes its weights here
+        optimizer.step()
+
+        running_loss += loss.item()
+    else:
+        print("Epoch {} - Training loss: {}".format(e, running_loss / len(trainloader)))
+print("\nTraining Time (in minutes) =", (time() - time0) / 60)
+
+# testing process
+correct_count, all_count = 0, 0
+for images, labels in valloader:
+    for i in range(len(labels)):
+        img = images[i].view(1, 784)
+        with torch.no_grad():
+            logps = model(img)
+
+        ps = torch.exp(logps)
+        probab = list(ps.numpy()[0])
+        pred_label = probab.index(max(probab))
+        true_label = labels.numpy()[i]
+        if (true_label == pred_label):
+            correct_count += 1
+        all_count += 1
+
+print("Number Of Images Tested =", all_count)
+print("\nModel Accuracy =", (correct_count / all_count))
+
+# save model
+torch.save(model.state_dict(), './my_mnist_covolutional_model.pt')
